86f152c8aad424d902ccc247a6380e7f364dc7fc
Kevin Juliano <kevinjuliano.pires@gmail.com>
2023-10-26 20:49:44 -0300

    feat: image.network

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,30 +14,35 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            color: Colors.green,
-            width: 100,
-            height: 100,
-          ),
-          Container(
-            color: Colors.yellow,
-            width: 80,
-            height: 80,
-          ),
-          Container(
-            color: Colors.blue,
-            width: 50,
-            height: 50,
-          ),
-          Container(
-            color: Colors.white,
-            width: 50,
-            height: 10,
-          ),
-        ],
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.network(
+                'https://i.pinimg.com/564x/55/38/e4/5538e41cde2e0da3f3369145af760ee9.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              color: Colors.yellow,
+              width: 80,
+              height: 80,
+            ),
+            Container(
+              color: Colors.blue,
+              width: 50,
+              height: 50,
+            ),
+            Container(
+              color: Colors.white,
+              width: 50,
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
